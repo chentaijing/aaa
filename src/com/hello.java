c2e70466dad05bcf0ba11bbea38a512bd44b24a8
chen <863097380@qq.com>
2020-09-11 12:11:06 +0800

    阿达

--- src/com/hello.java
+++ src/com/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("啊飒飒多所");
         System.out.println("啊飒飒多所");
+        System.out.println("啊飒飒多所");
+        System.out.println("啊飒飒多所");
     }
 }
